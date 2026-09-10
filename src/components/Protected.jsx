ed030a8b7e8826a148c874be7100c6af5db45c6b
tufail abbas <tufailabbas949@gmail.com>
2026-09-10 07:41:08 -0700

    api url erroe solve

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -8,7 +8,7 @@ const ProtectedRoute = ({ children }) => {
 const API_URL = import.meta.env.VITE_API_URL;
   useEffect(() => {
     axios
-      .get(`${API_URL}auth/me`, {
+      .get(`${API_URL}/auth/me`, {
         withCredentials: true,
       })
       .then(() => {
